b6fe278aab05888c28d175f8b4839baf5dcec84e
Jerónimo Restrepo <jrestrepoc@eafit.edu.co>
2025-08-01 11:13:20 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    #return HttpResponse("<h1>Welcome to the Movie Reviews Home Page!<h1>")
+    #return render(request, 'home.html')
+    return render(request, 'home.html', {'name': 'Greg Lim'})
+
+def about(request):
+    return HttpResponse("<h1>About Movie Reviews Page</h1>")
